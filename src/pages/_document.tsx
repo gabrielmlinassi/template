8aaf7c53b30b05068a338bb3ea93253d62dbab89
Gabriel <gabrielm.linassi@gmail.com>
2022-08-11 14:38:40 -0300

    Add a custom font sample

---
+++ src/pages/_document.tsx
@@ -0,0 +1,35 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/industry-600.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/proxima-nova-400.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/proxima-nova-600.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        industry: ['Industry', ...defaultTheme.fontFamily.sans],
+        proximaNova: ['Proxima Nova', ...defaultTheme.fontFamily.sans],
+      },
+      keyframes: {
+        // Dialog
+        'fade-in': {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+        'fade-out': {
+          '0%': { opacity: 1 },
+          '100%': { opacity: 0 },
+        },
+      },
+      animation: {
+        // Dialog
+        'fade-in': 'fade-in 0.2s ease',
+        'fade-out': 'fade-out 0.2s ease',
+      },
+    },
   },
   plugins: [
     require('tailwindcss-radix')({
